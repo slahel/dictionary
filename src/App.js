78632b131ac3e79cc1064402bfb621d8922702c5
slahel <74063357+slahel@users.noreply.github.com>
2022-02-28 22:21:45 +0100

    footer App.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,12 @@ function App() {
       <div>
         <h1>Dictionary</h1>
         <Dictionary />
+        <footer>
+          Coded by Stephanie Lahellec, check the open sources on{" "}
+          <a href="https://github.com/slahel/dictionary" target="_blank">
+            GitHub
+          </a>
+        </footer>
       </div>
     </div>
   );
