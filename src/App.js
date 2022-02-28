7200d118724e17914515e23649f3cf4c483ced76
slahel <74063357+slahel@users.noreply.github.com>
2022-02-28 22:12:54 +0100

    App.js changes

--- src/App.js
+++ src/App.js
@@ -4,8 +4,14 @@ import Dictionary from "./Dictionary";
 function App() {
   return (
     <div className="App">
-      <footer>Coded by Stephanie Lahellec</footer>
+      <h1>Dictionary</h1>
       <Dictionary />
+      <footer>
+        Coded by Stephanie Lahellec, check the open sources on{" "}
+        <a href="https://github.com/slahel/dictionary" target="_blank">
+          GitHub
+        </a>
+      </footer>
     </div>
   );
 }
