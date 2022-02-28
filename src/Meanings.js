85a5409377988e9f94a6c77024fc5c2eff7f827a
slahel <74063357+slahel@users.noreply.github.com>
2022-02-28 22:41:29 +0100

    Css fixes

--- src/Meanings.js
+++ src/Meanings.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Meaning(props) {
   return (
     <div className="Meaning">
-      <div>{props.meaning.partOfSpeech}</div>
+      <h4>{props.meaning.partOfSpeech}</h4>
       {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
--- src/Results.js
+++ src/Results.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Meaning from "./Meanings";
+import "./Results.css";
 
 export default function Results(props) {
   // console.log(props.results);
   if (props.results) {
     return (
       <div className="Results">
-        <h3>{props.results.word}</h3>
+        <h2>{props.results.word}</h2>
         <p className="phonetic">{props.results.phonetic}</p>
         {props.results.meanings.map(function (meaning, index) {
           return (
