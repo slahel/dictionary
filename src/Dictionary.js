3d5ccea7c2db7a44a4fca1e5eb95029a4b62bf07
slahel <74063357+slahel@users.noreply.github.com>
2022-03-03 14:06:27 +0100

    set Welcome as default word

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -7,38 +7,53 @@ import { RiSearchEyeFill } from "react-icons/ri";
 export default function Dictionary() {
   let [searchWord, setSearchWord] = useState("welcome");
   let [results, setResults] = useState(null);
+  let [loaded, setLoaded] = useState(false);
+
   function getResponse(response) {
     //console.log(response.data[0]);
     //console.log(response.data[0].meanings);
     setResults(response.data[0]);
   }
-  function handleSearcWord(event) {
+  function handleSearchWord(event) {
     setSearchWord(event.target.value);
   }
   function handleSubmit(event) {
     event.preventDefault();
+    Search();
+  }
+  function load() {
+    setLoaded(true);
+    Search();
+  }
+
+  function Search() {
     // api from: https://dictionaryapi.dev/
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${searchWord}`;
     axios.get(apiUrl).then(getResponse);
   }
-  return (
-    <div className="dictionary">
-      <header>
-        <h1>
-          <RiSearchEyeFill />
-          {"  "}
-          Dictionary
-        </h1>
-        <form onSubmit={handleSubmit}>
-          <input
-            className="searchBar"
-            type="search"
-            onChange={handleSearcWord}
-          ></input>
-          <input className="btn" type="submit"></input>
-        </form>
-      </header>
-      <Results results={results} />
-    </div>
-  );
+  if (loaded) {
+    return (
+      <div className="dictionary">
+        <header>
+          <h1>
+            <RiSearchEyeFill />
+            {"  "}
+            Dictionary
+          </h1>
+          <form onSubmit={handleSubmit}>
+            <input
+              className="searchBar"
+              type="search"
+              onChange={handleSearchWord}
+            ></input>
+            <input className="btn" type="submit"></input>
+          </form>
+        </header>
+        <Results results={results} />
+      </div>
+    );
+  } else {
+    load();
+    return ``;
+  }
 }
