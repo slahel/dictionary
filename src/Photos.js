ecb5adf0af68fc2f9edbfe9a8eb58c6b019779ab
slahel <74063357+slahel@users.noreply.github.com>
2022-03-03 17:26:46 +0100

    Update Photos.js

--- src/Photos.js
+++ src/Photos.js
@@ -10,7 +10,11 @@ export default function Photos(props) {
           {props.photos.map(function (photo, index) {
             return (
               <div className="wrapper" key={index}>
-                <a href={photo.src.original} target="_blank" rel="no_referrer">
+                <a
+                  href={photo.src.original}
+                  target="_blank"
+                  rel="noopener no_referrer"
+                >
                   <img
                     src={photo.src.tiny}
                     alt={photo.alt}
